8c76737aa4cc99e8ed1c2bef615db3f8104b77c0
hasankarim18 <hallysust@gmail.com>
2024-02-28 20:42:06 +0600

    button

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Button from "./ui/Button";
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-screen flex justify-center items-center">
+        <Button>On click</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
